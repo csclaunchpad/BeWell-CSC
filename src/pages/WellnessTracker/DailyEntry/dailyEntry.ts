18ac86f4cafa3d95f0f9c5d45062303694ef1f22
Tim <tim.jodoin@gmail.com>
2018-01-30 17:52:57 -0500

    daily checker range defaults

--- src/pages/WellnessTracker/DailyEntry/dailyEntry.ts
+++ src/pages/WellnessTracker/DailyEntry/dailyEntry.ts
@@ -1,53 +1,16 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
-
-
-export class BasicPage {
-  brightness: number = 20;
-  contrast: number = 0;
-  warmth: number = 1300;
-  structure: any = { lower: 33, upper: 60 };
-  text: number = 0;
-}
-
+import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-dailyEntry',
   templateUrl: 'dailyEntry.html'
 })
 export class DailyEntry {
-  brightness: number = 20;
-  contrast: number = 0;
-  warmth: number = 1300;
-  structure: any = { lower: 33, upper: 60 };
-  text: number = 0;    
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
+    public mood = 5;
+    public sleep = 5;
+    public diet = 5;
+    public stress = 5;
+    
+    constructor(public navCtrl: NavController ) {
     }
-  }
-
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(DailyEntry, {
-      item: item
-    });
-  }
 }
