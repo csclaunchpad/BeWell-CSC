4fbb55a0641d799c35ca1a582e283af6004e817d
jwams <justin.wams@csc-scc.gc.ca>
2018-08-31 10:21:05 -0700

    Changing root page for ionic pro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ import { SelfAssessment } from '../pages/Tools/assessment/selfassessment/selfAss
 export class MyApp {
     @ViewChild(Nav) nav: Nav;
 
-    rootPage: any = WellnessTracker;
+    rootPage: any = LanguageSelection;
 
 //    rootPage: any = HomePage;
 //    rootPage: any = Resources;
